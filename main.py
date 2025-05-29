64055624c3d8542d7e8a17a9126da327f3b65e72
shivensh <shivenshmeena0752@gmail.com>
2025-05-29 22:30:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -541,8 +541,7 @@ async def upload(bot: Client, m: Message):
                             # Clean up the downloaded file
                             if os.path.exists(f'{name}.jpg'):
                                 os.remove(f'{name}.jpg')         
-
-                 """       
+                              
                 elif "youtu" in url: 
                     try:
                         await bot.send_photo(chat_id=m.chat.id, photo=photo, caption=cyt)
@@ -551,7 +550,7 @@ async def upload(bot: Client, m: Message):
                         await m.reply_text(str(e))
                         await asyncio.sleep(1)
                         continue
-                    """
+            
                 elif ".ws" in url and  url.endswith(".ws"):
                         try : 
                             await helper.pdf_download(f"{api_url}utkash-ws?url={url}&authorization={api_token}",f"{name}.html")
